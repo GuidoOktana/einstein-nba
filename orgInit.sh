d7805e18fc72170ef984192df9c15fd6f4df2238
GuidoOktana <guido@oktana.io>
2020-01-30 11:02:31 -0300

    test data fetch

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,8 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx shane:github:package:install -g GuidoOktana -r einstein-demos
+curl https://github.com/GuidoOktana/einstein-demos/data
 sfdx force:source:push
-sfdx force:user:permset:assign -n NBA
+sfdx force:user:permset:assign -n ED
 sfdx shane:data:tree:import -p data/plan.json -d data/
 sfdx force:user:password:generate
 sfdx force:org:open
