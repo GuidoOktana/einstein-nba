e31c5d2975b4697d80450b85837e2fe9a124fd81
GuidoOktana <guido@oktana.io>
2020-01-30 14:00:06 -0300

    push instead of deploy

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx shane:github:package:install -g GuidoOktana -r einstein-demos
 # //get data
-sfdx force:source:deploy
+sfdx force:source:push
 sfdx force:user:permset:assign -n ED
 sfdx shane:data:tree:import -p data/plan.json -d data/
 sfdx force:user:password:generate
