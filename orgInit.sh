d1f2b129bfe4c9a5dba7225661c9ee3188e94f58
GuidoOktana <guido@oktana.io>
2020-01-30 13:35:07 -0300

    scripts update

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,8 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx shane:github:package:install -g GuidoOktana -r einstein-demos
-curl https://github.com/GuidoOktana/einstein-demos/data
-sfdx force:source:push
+# //get data
+sfdx force:source:deploy
 sfdx force:user:permset:assign -n ED
-sfdx shane:data:tree:import -p data/plan.json -d data/
+# sfdx shane:data:tree:import -p data/plan.json -d data/
 sfdx force:user:password:generate
 sfdx force:org:open
