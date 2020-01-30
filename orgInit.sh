2e3f86a8d9a00da987acdaf23350da03a3f0dcdf
GuidoOktana <guido@oktana.io>
2020-01-30 13:54:39 -0300

    data in nba

--- orgInit.sh
+++ orgInit.sh
@@ -3,6 +3,6 @@ sfdx shane:github:package:install -g GuidoOktana -r einstein-demos
 # //get data
 sfdx force:source:deploy
 sfdx force:user:permset:assign -n ED
-# sfdx shane:data:tree:import -p data/plan.json -d data/
+sfdx shane:data:tree:import -p data/plan.json -d data/
 sfdx force:user:password:generate
 sfdx force:org:open
